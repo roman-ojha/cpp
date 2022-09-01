1572417e3e1bf31bc52f78cdaf6b938bdf0b95a8
Roman-Ojha <razzroman98@gmail.com>
2022-09-01 22:27:15 +0545

    docs: OS Linux Basic Command

---
+++ Collage/Operating_System/01_Linux_Basic_Command/01.cpp
@@ -0,0 +1,20 @@
+/*
+LINUX BASIC COMMANDS
+Some of the basic linux commands are:
+a) pwd : print working directory, it will print path to your current directory
+b) ls: provide list of names of files and directories
+c) cd : Change your current directory eg: cd desktop
+d) mkdir : to create a new directory. eg: mkdirsoch
+e) rm -d : delete the directory. eg: rm -rf soch (for kali linux OS) rm -d soch (for ubuntu )
+FILE MANIPULATION COMMANDS
+a)cat filename : to view files. This will print out entire contents of file to terminal
+eg:cat pokhara
+b) touch filename : to create an empty file with name given in current directory.
+eg: touch infomax
+c) nano filename: to edit the contents of file. To save press CTRL + X. and enter.
+GCC COMPILER
+$ gcc -o filename filename.c
+$ ./ filename
+sudo apt-get update : to update ubuntu version
+sudo apt-get install gcc : compiler for c and c++ Eg: $ gcc -o add add.c $ ./ add
+*/
