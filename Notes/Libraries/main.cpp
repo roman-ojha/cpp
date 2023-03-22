456c42c0ef49fbde0ebb9f518e36b6398b938b44
Roman-Ojha <razzroman98@gmail.com>
2023-03-22 15:09:42 +0545

    build: init project for using dynamic libraries

---
+++ Notes/Libraries/main.cpp
@@ -0,0 +1,15 @@
+/*
+    *) Using Dynamic Libraries
+        -> Dynamic libraries are a binary files libraries that are loaded at runtime.
+        -> They are not linked into the executable at compile time.
+        -> They are loaded into the process at runtime.
+        -> dynamic linking help to make you application optimize
+        -> it is not part of the executable file, so when you launch the executable we and load the dynamic libraries into the executable
+        -> there is a two way you can load dynamic libraries:
+            1. load it on run times:
+                -> when you launch the executable at that time we will load the dynamic libraries.
+                -> so, executable file is aware of the dynamic library it have to load
+            2. dynamic linking:
+                -> we will write a code inside the cpp file where we will load the dynamic libraries as per our requirement
+    *) Load Dynamic libraries on run times:
+*/
