fc442d6a5d64d2ebbb39ee3f103b34d4ffc795ec
Roman-Ojha <razzroman98@gmail.com>
2023-03-22 17:31:31 +0545

    feat: declaring FibLibrary.h to create dll using gcc

---
+++ Notes/Libraries/07_Create_and_Use_Dynamic_Libraries/FibLibrary.h
@@ -0,0 +1,29 @@
+#pragma once
+
+// you have to define this to be able to generate '.lib', '.dll', '.exp' files
+// first we will define the macro
+#ifdef FIBLIBRARY
+#define FIBLIBRARY
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+#ifdef BUILD_MY_DLL // this option causes the dll functions to export and available to client applications
+#define SHARED_LIB __declspec(dllexport)
+#else
+#define SHARED_LIB __declspec(dllimport)
+#endif
+
+    // and you have to declare like this to be able to generate '.lib', '.dll', '.exp'
+    // and all of these files are related to each other
+    int SHARED_LIB fib(int n);
+
+#ifdef __cplusplus
+}
+#endif
+
+// declaring the another function outside the extern "C" block
+void SHARED_LIB helloWorld();
+
+#endif // end of DLL
